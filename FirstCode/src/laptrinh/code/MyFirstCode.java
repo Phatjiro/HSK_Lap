d38487fa0cc754f9172a87fbf6074ba6207eb88e
Phatjiro <nguyenvanphat911@gmail.com>
2022-03-04 00:01:10 +0700

    commit dau tien - gui file len github

---
+++ FirstCode/src/laptrinh/code/MyFirstCode.java
@@ -0,0 +1,8 @@
+package laptrinh.code;
+
+public class MyFirstCode {
+	public static void main(String[] args) {
+		System.out.println("Máy tính mới nè mọi người ới!! <3");
+		System.out.println("Yêu mẹ nhiều <3 <3");
+	}
+}
